5c62e88e35d0131f65c97ae36604b76505b48bdb
nikakarelets <v.karelets@gmail.com>
2021-10-20 20:40:58 +0300

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# React Uber Eats
-[LINK](https://nikakarelets.github.io/react_uber-eats/)
---
+++ precache-manifest.162b3f6f19fdc48fd88d30efb5726857.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44bea4b07b82f42f3036f767db17be32",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "71c30ef8c2fc866c1ab0",
+    "url": "/react_uber-eats/static/css/main.14ef9444.chunk.css"
+  },
+  {
+    "revision": "a62cff03170f504c40bb",
+    "url": "/react_uber-eats/static/js/2.b031e3a3.chunk.js"
+  },
+  {
+    "revision": "71c30ef8c2fc866c1ab0",
+    "url": "/react_uber-eats/static/js/main.80e487ef.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);
